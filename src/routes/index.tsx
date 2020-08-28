b78d17c51bc4c7796fafc240e82ccaff26c1a532
José Neto <josenetopr5@gmail.com>
2020-08-28 15:13:28 -0300

    Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Search from '../pages/Search';
+import Details from '../pages/Details';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/search" component={Search} />
+      <Route path="/details" component={Details} />
+    </Switch>
+  );
+}
+
+export default Routes;
