3777ceb3a1bbdc322cab807c1a82b65ed17fc69c
José Neto <josenetopr5@gmail.com>
2020-08-30 00:15:11 -0300

    Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Header from '../../components/Header';
 
+import { Container, Content } from './styles';
+import { FiArrowRight } from 'react-icons/fi';
 import homeImg from '../../assets/homeImg.svg';
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <Header />
-      <img src={homeImg} alt="Home" />
+
+      <Container>
+        <Content>
+          <div className="welcome">
+            <h2>O que você quer ler hoje?</h2>
+
+            <p>
+              Encontre os livros que você procura digitando o
+              <span> nome do livro</span>, <span>autor</span> ou
+              <span> editora</span>.
+            </p>
+
+              <button className='search'>
+            <Link to="/search">
+                Pesquisar
+                <FiArrowRight />
+            </Link>
+              </button>
+          </div>
+
+          <div className="home-img">
+            <img src={homeImg} alt="Home" />
+          </div>
+
+        </Content>
+      </Container>
     </>
   );
 };
