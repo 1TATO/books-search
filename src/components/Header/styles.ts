b12855e24919c0535f92ceb0ec04ec541581f3d7
José Neto <josenetopr5@gmail.com>
2020-08-30 21:37:11 -0300

    Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const Title = styled.header`
-  font-size: 24px;
+  font-size: 1.4rem;
 
   a {
     text-decoration: none;
 
     h1 {
-      color: #59AFE6;
+      color: var(--primary-color);
     }
   }
 `;
