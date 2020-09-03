7e3e18065a45ad5a0e35ec9d806a6df5d63de56d
José Neto <josenetopr5@gmail.com>
2020-09-03 13:57:38 -0300

    Details page

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import Header from '../../components/Header';
 
 import { Container, BookDetails } from './styles';
+import notFound from '../../assets/not-found.svg';
+import { FiArrowLeft } from 'react-icons/fi';
 
 interface BookParams {
   id: string;
@@ -50,20 +52,39 @@ const Details: React.FC = () => {
           <BookDetails>
             <h2>{book.volumeInfo.title}</h2>
 
-            <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.volumeInfo.title} />
+            {book.volumeInfo.imageLinks !== undefined
+              ? <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.volumeInfo.title} />
+              : <img src={notFound} alt="Book cover not found" />
+            }
 
             <div className="info">
               <p><span>Autores: </span>{book.volumeInfo.authors}</p>
-              <p><span>Editora: </span>{book.volumeInfo.publisher}</p>
-              <p><span>Paginas: </span>{book.volumeInfo.pageCount}</p>
-              <p><span>Idioma: </span>{book.volumeInfo.language}</p>
-              <p><span>Classificacao media: </span>{book.volumeInfo.averageRating} - {book.volumeInfo.ratingsCount} avaliacoes</p>
+              {book.volumeInfo.publisher !== undefined
+                ? <p><span>Editora: </span>{book.volumeInfo.publisher}</p>
+                : ''
+              }
+              {book.volumeInfo.pageCount !== undefined
+                ? <p><span>Páginas: </span>{book.volumeInfo.pageCount}</p>
+                : ''
+              }
+              {book.volumeInfo.language !== undefined
+                ? <p><span>Idioma: </span>{book.volumeInfo.language}</p>
+                : ''
+              }
+              {book.volumeInfo.averageRating !== undefined
+                ? <p><span>Classificação média: </span>{book.volumeInfo.averageRating} - {book.volumeInfo.ratingsCount} avaliações</p>
+                : ''
+              }
             </div>
 
             <a href={book.volumeInfo.infoLink} target="__blank">Amostra</a>
             <a href={book.saleInfo.buyLink} target="__blank">Comprar</a>
           </BookDetails>
         )}
+
+        <Link to="/search">
+          <FiArrowLeft />
+        </Link>
       </Container>
     </>
   );
